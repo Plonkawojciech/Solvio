cf89d4c7ad7e30b532771571008d8d9660b1edd2
AveJaPl <filip.piatek2005@gmail.com>
2025-10-28 18:56:28 +0100

    backup before Tailwind 4 migration

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,14 +1,17 @@
-import { DeployButton } from '@/components/deploy-button'
-import { EnvVarWarning } from '@/components/env-var-warning'
-import { AuthButton } from '@/components/auth-button'
-import { ThemeSwitcher } from '@/components/theme-switcher'
-import { hasEnvVars } from '@/lib/utils'
-import Link from 'next/link'
+// app/(protected)/layout.tsx
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/protected/main/sidebar"
 
-export default function ProtectedLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <>{children}</>
+export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="pl-[var(--sidebar-width)] p-4">
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+
+
+  )
 }
--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,14 +1,17 @@
-import { redirect } from 'next/navigation'
+// import { redirect } from 'next/navigation'
 
-import { createClient } from '@/lib/supabase/server'
+// import { createClient } from '@/lib/supabase/server'
 
 export default async function ProtectedPage() {
-  const supabase = await createClient()
+  // const supabase = await createClient()
 
-  const { data, error } = await supabase.auth.getClaims()
-  if (error || !data?.claims) {
-    redirect('/auth/login')
-  }
+  // const { data, error } = await supabase.auth.getClaims()
+  // if (error || !data?.claims) {
+  //   redirect('/auth/login')
+  // }
 
-  return <></>
+  return <>
+    <h1>Protected Page</h1>
+  </>
+
 }
