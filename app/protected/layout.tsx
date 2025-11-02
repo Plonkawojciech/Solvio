93be90005eebb8b81a63f5e4eb6dc53d4c9b0cb9
AveJaPl <filip.piatek2005@gmail.com>
2025-11-02 13:01:59 +0100

    layout czanges

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,19 +1,25 @@
 import { AppSidebar } from '@/components/protected/main/sidebar'
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
-import Link from 'next/link'
+// Usunąłem nieużywany import 'Link'
 
 export default function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return <>
+  return (
     <SidebarProvider>
-      <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        {children}
-      </main>
+      <div className="flex min-h-screen">
+        <AppSidebar />
+        <main className="flex-1 overflow-auto p-6 md:p-10">
+          
+       
+          <div className="md:hidden mb-4">
+            <SidebarTrigger />
+          </div>
+          {children}
+        </main>
+      </div>
     </SidebarProvider>
-  </>
+  )
 }
