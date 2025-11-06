ab455c74270ade6f7c037b31f501595558ae6f51
AveJaPl <filip.piatek2005@gmail.com>
2025-11-06 17:38:10 +0100

    Quick fix  - header on protected pages

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -3,6 +3,7 @@ import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import { redirect } from 'next/navigation'
 import { createClient } from '@/lib/supabase/server'
 import { Toaster } from 'sonner'
+import Header from '@/components/header'
 
 export default async function ProtectedLayout({
   children,
@@ -18,15 +19,18 @@ export default async function ProtectedLayout({
 
   return (
     <SidebarProvider>
-      <div className="flex min-h-screen">
-        <AppSidebar />
-        <main className="flex-1 overflow-auto p-6 md:p-10">
-          <div className="md:hidden mb-4">
-            <SidebarTrigger />
-          </div>
-          {children}
-          <Toaster position="top-right" richColors />
-        </main>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <div className="flex flex-1 overflow-hidden">
+          <AppSidebar />
+          <main className="flex-1 overflow-auto p-6 md:p-10">
+            <div className="md:hidden mb-4">
+              <SidebarTrigger />
+            </div>
+            {children}
+            <Toaster position="top-right" richColors />
+          </main>
+        </div>
       </div>
     </SidebarProvider>
   )
